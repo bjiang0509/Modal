6d4720763aff48dc8b014ffae713c548a0f147e8
Bijun Jiang <86031227+bjiang0509@users.noreply.github.com>
2022-05-03 16:42:14 -0700

    Update script.js

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+//returns a node list containing all elements with class 'show-modal'
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const showModal = e => {
+  let activeModal = e.target;
+  modal.classList.remove('hidden');
+  console.log(e.target.className);
+};
+const hideModal = e => {
+  modal.classList.add('hidden');
+};
+
+for (let btnOpenModal of btnsOpenModal) {
+  btnOpenModal.addEventListener('click', showModal);
+}
+btnCloseModal.addEventListener('click', hideModal);
